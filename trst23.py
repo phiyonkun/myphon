cd6901fd072787e5ef33cd564e232a1dfb70abe4
Phiyongkun Trakunjantrasiri <traphiyongkun@gmail.com>
2025-09-05 03:59:26 +0000

    mix

---
+++ trst23.py
@@ -0,0 +1,26 @@
+# คำสั่ง for (for-in)
+...
+for var_name in range(n) :
+คำสั่ง (s)
+for var_name in range(n, m) :
+คำสั่ง (s)
+for var_name in range(n, m, o) :
+คำสั่ง (s)
+for var_name in STRING :
+คำสั่ง (s)
+
+
+
+
+
+# for aa in range(5) :
+# print(aa + 1, 'Hello')
+
+# for aa in range(3, 7) :
+# print(aa, 'Hello')
+
+# for aa in range(5) :
+# print(aa + 1, 'Hello')
+
+for aa in range(2, 10, 3) :1
+print(aa, 'Hello')
